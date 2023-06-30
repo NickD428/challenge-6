8080238a497ca92144f1de81d32f2dd7bcba4381
Nick <chargerfan428@aol.com>
2023-06-29 19:10:09 -0700

    added my API key and functions to javascript file

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const apiKey = 'a57f017885ca4e1fbab263ece353c473'; 
+
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const currentWeatherDiv = document.getElementById('current-weather-info');
+const forecastDiv = document.getElementById('forecast-info');
