60e770e1299f054b673c60f76abf2ecf8b499308
Nick <chargerfan428@aol.com>
2023-06-29 19:17:49 -0700

    added event listener and get weather function

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,27 @@ const searchForm = document.getElementById('search-form');
 const cityInput = document.getElementById('city-input');
 const currentWeatherDiv = document.getElementById('current-weather-info');
 const forecastDiv = document.getElementById('forecast-info');
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+    if (city !== '') {
+      getWeather(city);
+    }
+  });
+  
+  function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      .then(response => response.json())
+      .then(data => {
+        displayCurrentWeather(data);
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
+      })
+      .then(response => response.json())
+      .then(data => {
+        displayForecast(data);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+  }
